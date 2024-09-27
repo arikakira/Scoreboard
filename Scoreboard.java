564c469f836f4cecf3cadf6ec5024d99b35fc896
arikakira <samanthal242@nycstudents.net>
2024-09-26 22:47:04 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,9 +1,16 @@
 public class Scoreboard{
     private String teamOne;
     private String teamTwo;
-    
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+
     public Scoreboard(String one, String two) {
         teamOne = one;
         teamTwo = two;
+    }
+
+    public String getScore(){
+        return null;
     }
 }
