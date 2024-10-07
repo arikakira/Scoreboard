0d11d0da6b9c518742b5f6f081dfd9efafaca692
arikakira <samanthal242@nycstudents.net>
2024-10-07 11:40:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
+
+        info = game.getScore();
+        System.out.println(info);
+
         game.recordPlay(1);
         info = game.getScore();
         System.out.println(info);
@@ -16,15 +20,23 @@ public class Main
         info = game.getScore();
         System.out.println(info);
 
-        game.recordPlay(2);
-        info = game.getScore();
-        System.out.println(info);
+        game.recordPlay(1);
 
         game.recordPlay(0);
         info = game.getScore();
         System.out.println(info);
 
+        game.recordPlay(0);
+
         game.recordPlay(4);
+
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        info = match.getScore();
+        System.out.println(info);
         info = game.getScore();
         System.out.println(info);
     }
